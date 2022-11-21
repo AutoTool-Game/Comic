d09c487631407afb5b48c3a8742bd5c9015f4a97
Toan Vu Duc <toanvd@vega.com.vn>
2022-11-21 14:35:44 +0700

    message

---
+++ pages/category.vue
@@ -0,0 +1,20 @@
+<template>
+  <component
+    :is="nameComponent"
+  />
+</template>
+
+<script>
+import pageMixin from '~/mixins/pageMixin'
+
+export default {
+  name: "category",
+  components: {
+    DesktopComponent: () => import('@/components/desktop/pages/CategoryPage'),
+    MobileComponent: () => import('@/components/mobile/pages/CategoryPage')
+  },
+  mixins: [
+    pageMixin
+  ],
+}
+</script>
